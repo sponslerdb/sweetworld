52944e76e76894e2c91c4d6f3448ac0bfa94c17e
Doug Sponsler <sponslerdb@gmail.com>
2021-10-27 11:38:47 +0200

    renamed to "sweetworld"

--- inst/extdata/stickerscript.R
+++ inst/extdata/stickerscript.R
@@ -1,11 +1,11 @@
 library(magick)
-s <- image_read("~/floralcurves/inst/extdata/hex_sticker1_baseplot.png")
+s <- image_read("~/sweetworld/inst/extdata/hex_sticker1_baseplot.png")
 
-sticker(s, package="nectar", p_size=20, 
-        s_x=1, 
-        s_y=1, 
-        s_width=1.5, 
-        s_height=1.5, 
+sticker(s, package="sweetworld", p_size=20,
+        s_x=1,
+        s_y=1,
+        s_width=1.5,
+        s_height=1.5,
         p_y = 1.05,
         p_color = "forestgreen",
-        h_fill="white", h_color="forestgreen", filename="~/floralcurves/inst/extdata/hex_sticker1.png")
+        h_fill="white", h_color="forestgreen", filename="~/sweetworld/inst/extdata/hex_sticker1.png")
